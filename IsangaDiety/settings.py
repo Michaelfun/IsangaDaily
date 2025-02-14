65701cd73ff85e1052a756f4811af87cca1d2275
michaelfun <funganoti@gmail.com>
2025-02-14 03:28:47 +0300

    new updates

--- IsangaDiety/settings.py
+++ IsangaDiety/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8q^$%*$i_7%s1ehpf54_#f9no42g@@-p++^f-kwr4x7)i9g($$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'isangadata.pythonanywhere.com ']
+ALLOWED_HOSTS = ['.vercel.app','localhost','127.0.0.1','.now.sh']
 
 
 # Application definition
@@ -80,12 +80,23 @@ WSGI_APPLICATION = 'IsangaDiety.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'VASifmpoREZHzGjfoknHCqVreQsfxTkZ',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '47095',  
     }
-}
+} 
 
 
 # Password validation
