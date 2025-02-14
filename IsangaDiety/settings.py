22ee2f7e489f9dfde4bc4cc60171ec5648647eca
michaelfun <funganoti@gmail.com>
2025-02-14 03:48:43 +0300

    new updates

--- IsangaDiety/settings.py
+++ IsangaDiety/settings.py
@@ -137,7 +137,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
